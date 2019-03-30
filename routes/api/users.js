7761d16111fba46f0710088cb6c3f09e241a48b6
Sharath19940604 <43935070+Sharath19940604@users.noreply.github.com>
2019-03-30 09:53:21 +0000

    dashboard changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -89,7 +89,7 @@ router.post("/login",(req, res) => {
             payload,
             keys.secretOrKey,
             {
-              expiresIn: 3600 // 1 year in seconds
+              expiresIn: 3600
             },
             (err, token) => {
               res.json({
