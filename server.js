16b3e6f368f0e5dc4cafefa9605a0ea56be3e2e4
Sharath19940604 <43935070+Sharath19940604@users.noreply.github.com>
2019-05-02 19:46:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,8 @@ app.use("/api/users", users);
 app.use("/api/profile",profile);
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
