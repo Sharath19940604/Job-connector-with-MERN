7b0cd2cb7e9fedd7943beafbd43f98dadf35c121
Sharath19940604 <43935070+Sharath19940604@users.noreply.github.com>
2019-05-02 19:45:46 +0100

    Update Profiles.js

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -6,8 +6,35 @@ import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profileActions';
 
 class Profiles extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { inputValue: '', inputLocationValue: '' };
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.updateInputLocationValue = this.updateInputLocationValue.bind(this);
+  }  
+
   componentDidMount() {
     this.props.getProfiles();
+  }
+
+  getInitialState = function(){
+    return {
+      inputValue: '',
+      inputLocationValue : ''
+    };
+  }
+
+  updateInputValue = function(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+  updateInputLocationValue = function(evt) {
+    this.setState({
+      inputLocationValue: evt.target.value
+    });
   }
 
   render() {
@@ -17,11 +44,18 @@ class Profiles extends Component {
     if (profiles === null || loading) {
       profileItems = <Spinner />;
     } else {
-      if (profiles.length > 0) {
+      if (profiles.length > 0 && this.state.inputValue === '' && this.state.inputLocationValue === '') {
         profileItems = profiles.map(profile => (
           <ProfileItem key={profile._id} profile={profile} />
         ));
-      } else {
+      } else if(profiles.length > 0 && this.state.inputValue !== '' && this.state.inputLocationValue === ''){
+        profileItems = profiles.map(profile => (profile.user.name.toLowerCase().includes(this.state.inputValue.toLowerCase())) ? <ProfileItem key={profile._id} profile={profile} />  : '');
+      } else if(profiles.length > 0 && this.state.inputValue === '' && this.state.inputLocationValue !== ''){
+        profileItems = profiles.map(profile => (profile.location.toLowerCase().includes(this.state.inputLocationValue.toLowerCase())) ? <ProfileItem key={profile._id} profile={profile} />  : '');
+      } else if(profiles.length > 0 && this.state.inputValue !== '' && this.state.inputLocationValue !== ''){
+        profileItems = profiles.map(profile => (profile.user.name.toLowerCase().includes(this.state.inputValue.toLowerCase()) && profile.location.toLowerCase().includes(this.state.inputLocationValue.toLowerCase())) ? <ProfileItem key={profile._id} profile={profile} />  : '');
+      } 
+      else{
         profileItems = <h4>No profiles found...</h4>;
       }
     }
@@ -35,6 +69,10 @@ class Profiles extends Component {
               <p className="lead text-center">
                 Browse and connect with developers
               </p>
+              <div className="card card-body bg-light mb-3">
+              <strong>Name : </strong><input type="text" placeholder="Search.." onChange={this.updateInputValue}></input>
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+              <strong>Location : </strong><input type="text" placeholder="Search.." onChange={this.updateInputLocationValue}></input> </div>
               {profileItems}
             </div>
           </div>
